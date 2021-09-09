1f4443d26d4fc1944fe802c7943b57741d2f8d32
shiyanshirani <shiyan99s@gmail.com>
2021-09-10 00:38:32 +0530

    renamed folder from packages to plugins

---
+++ plugins/monthly_savings.py
@@ -0,0 +1,23 @@
+import csv
+MONTHLY_INCOME = 200000
+
+def calculate(filepath, output):
+    with open(f'{filepath}') as file:
+        with open(f'output/{output}', 'a') as newfile:
+            fieldnames = ['name', 'age', 'phone', 'email', 'address', 'monthly_savings']
+            writer = csv.DictWriter(newfile, fieldnames=fieldnames)
+            writer.writeheader()
+            reader = csv.reader(file)
+            for row in reader:
+                if row[5] > MONTHLY_INCOME: # ValueError
+                    writer.writerow(
+                        {
+                            "name": row[0],
+                            "age": row[1],
+                            "phone": row[2],
+                            "email": row[3],
+                            "address": row[4],
+                            "monthly_savings": row[5],
+                        }
+                    )
+    print(f'{output}.csv file generated')
---
+++ plugins/send_sms.py
@@ -0,0 +1,28 @@
+import os, csv
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+
+# Outbound SMS
+def sendNotification(filepath):
+    print('sending notifcations')
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN') 
+
+    client = Client(account_sid, auth_token)
+    
+    with open(f'{filepath}', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            try:
+                client.messages.create(
+                    to=str(row[2]),
+                    from_="+14243532289",
+                    body="We have received your response. Thank you for being a part of our data drive."
+                )
+            except Exception as e:
+                print(e)
+                
+    print('Notifications sent to customers.')
